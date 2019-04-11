db699922ee6c9ea0b6fabe0963a43f23729d9944
zcgong <waterjack@126.com>
2019-04-11 19:18:28 +0800

    add contentType

--- AFNetworking/AFURLResponseSerialization.m
+++ AFNetworking/AFURLResponseSerialization.m
@@ -225,7 +225,7 @@ id AFJSONObjectByRemovingKeysWithNullValues(id JSONObject, NSJSONReadingOptions 
         return nil;
     }
 
-    self.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", nil];
+    self.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", @"text/html", @"text/plain", @"application/octet-stream",nil];
 
     return self;
 }
